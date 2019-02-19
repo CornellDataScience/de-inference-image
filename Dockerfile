e1fb8757610104329823850c7bc1eb231d622e26
Jo Chuang <josephch405@gmail.com>
2019-02-19 00:45:49 -0500

    Add new requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM jfloff/alpine-python:3.6-slim
 
+RUN sudo apt install -y cmake
+
 ADD server.py .
 CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -1,29 +1,33 @@
-from http.server import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import face_recognition
 
 PORT_NUMBER = 8080
 
+
 class myHandler(BaseHTTPRequestHandler):
-    #Handler for the GET requests
+    # Handler for the GET requests
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
         # Send the html message
         self.wfile.write("Hello World !".encode())
         return
+
     def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write('{"hi": "hi"}'.encode())
 
-try:
-    #Create a web server and define the handler to manage the
-    #incoming request
-    server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print('Started httpserver on port ' , PORT_NUMBER)
 
-    #Wait forever for incoming http requests
+try:
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print('Started httpserver on port ', PORT_NUMBER)
+
+    # Wait forever for incoming http requests
     server.serve_forever()
 
 except KeyboardInterrupt:
